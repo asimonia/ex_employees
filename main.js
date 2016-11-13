1874dca34ff689f2ceb6b8d76da3b5f889f99863
asimonia <alex.simonian@gmail.com>
2016-11-12 23:43:11 -0600

    Complete

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ let Operations = function() {
 	this.responsibility = null;
 };
 
+Clinician.prototype = new Employee();
+Operations.prototype = new Employee();
+
 Clinician.prototype.SetExpertise = function(expertise) {
 	this.expertise = expertise;
 };
@@ -29,8 +32,7 @@ Operations.prototype.SetResponsibility = function(resp) {
 };
 
 
-Clinician.prototype = new Employee();
-Operations.prototype = new Employee();
+
 
 let Department = function() {
 	this.deptName = null;
@@ -44,26 +46,60 @@ let FieldOffice = function() {
 	this.city = null;
 };
 
-FieldOffice.prototype.SetCity = function(city) {
-	this.city = city;
-};
-
 let Headquarters = function() {
 	this.hqName = null;
+};
+
+FieldOffice.prototype = new Department();
+Headquarters.prototype = new Department();
+
+FieldOffice.prototype.SetCity = function(city) {
+	this.city = city;
 };
 
 Headquarters.prototype.SetHQName = function(name) {
 	this.hqName = name;
 };
 
-FieldOffice.prototype = new Department();
-Headquarters.prototype = new Department();
+let ee01 = new Clinician();
+ee01.SetEmployeeFirstName("Alex");
+ee01.SetEmployeeLastName("Simonian");
+ee01.SetExpertise("Number 1 Stunna");
+
+let ee01_dept = new FieldOffice();
+ee01_dept.SetCity("San Diego");
+ee01.department = ee01_dept;
+
+let ee02 = new Operations();
+ee02.SetEmployeeFirstName("Jake");
+ee02.SetEmployeeLastName("Weary");
+ee02.SetResponsibility("Librarian");
+
+let ee02_dept = new Headquarters();
+ee02_dept.SetHQName("Dallas");
+ee02.department = ee02_dept;
+
+let ee03 = new Clinician();
+ee03.SetEmployeeFirstName("Joe");
+ee03.SetEmployeeLastName("Schmoe");
+ee03.SetExpertise("Surgeon");
+
+let ee03_dept = new FieldOffice();
+ee03_dept.SetCity("Topeka");
+ee03.department = ee03_dept;
+
+let ee04 = new Operations();
+ee04.SetEmployeeFirstName("Harray");
+ee04.SetEmployeeLastName("Caray");
+ee04.SetResponsibility("Announcer");
+
+let ee04_dept = new Headquarters();
+ee04_dept.SetHQName("Chicago");
+ee04.department = ee04_dept;
 
 
+console.log(`${ee01.first} ${ee01.last} works as a clinician, in the ${ee01.department.city} office, and is a ${ee01.expertise}`);
+console.log(`${ee02.first} ${ee02.last} works as an operative, in the ${ee02.department.hqName} office, and is a ${ee02.responsibility}`);
+console.log(`${ee03.first} ${ee03.last} works as a clinician, in the ${ee03.department.city} office, and is a ${ee03.expertise}`);
+console.log(`${ee04.first} ${ee04.last} works as an operative, in the ${ee04.department.hqName} office, and is a ${ee04.responsibility}`);
 
-// Every employee should be assigned to either the FieldOffice or Headquarters department. This is a has-a relationship.
-// Create a minimum of 4 employees - 2 clinicians, and 2 operations - that have been assigned to a specific department.
-// Output the following information to the console for each employee.
-// Full name
-// Department name
-// Expertise/responsibility
